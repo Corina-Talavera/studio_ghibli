4e45be1b4e6fa2414a8308058c23779ab281db1a
Corina Talavera <43788757+Corina-Talavera@users.noreply.github.com>
2018-12-22 09:52:14 -0500

    Create api.rb

---
+++ lib/studio_ghibli/api.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+require 'nokogiri'
+require 'open-uri'
+require 'net/http'
+require 'rubygems'
+require 'json'
+
+class StudioGhibliAPI
+  def get_film
+   data = open('https://ghibliapi.herokuapp.com/films')
+   .read
+   JSON.parse(data)
+  end
+end 
+
+class Film
+  attr_accessor :title, :description, :rt_score
+  def initialize(title_hash)
+    title_hash.each do |method,arg|
+      if self.respond_to?("#{method}=")
+        self.send("#{method}=",arg) 
+      end   
+     end
+   end
+end  
+
+api = StudioGhibliAPI.new
+results = api.get_film.map do |title_hash|
+  Film.new(title_hash)
+end  
