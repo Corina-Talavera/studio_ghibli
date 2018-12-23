f6600326fb29b1696ae2b72845fd73e6e65247f1
Corina Talavera <43788757+Corina-Talavera@users.noreply.github.com>
2018-12-23 10:43:36 -0500

    Update film.rb

--- lib/studio_ghibli/film.rb
+++ lib/studio_ghibli/film.rb
@@ -1,1 +1,24 @@
+class StudioGhibli::Film
+  attr_accessor :title, :description, :rt_score,
 
+  @@all = []
+
+  def initialize(title, description, rt_score)
+    @title = title
+    @description = description
+    @rt_score = rt_score
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_by_index(index)
+    @@all[index]
+  end
+end
