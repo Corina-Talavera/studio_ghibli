f8c638944c00bb618cd267d3edb3d18778cf8481
Corina Talavera <43788757+Corina-Talavera@users.noreply.github.com>
2018-12-27 14:44:02 -0500

    Update cli.rb
    updated cli class naming

--- lib/studio_ghibli/cli.rb
+++ lib/studio_ghibli/cli.rb
@@ -1,55 +1,36 @@
-class StudioGhibli::CLI
+class CLI
+  
+  attr_reader :title, :description, :rt_score
+  
   def call
-    welcome
-    list_film
-    menu
-    goodbye
-  end
-
-  def welcome
-    puts ""
-    puts "** Welcome to the StudioGhibli CLI **"
-    puts ""
-  end
-
-  def list_film
-    StudioGhibli::Film.all.each do |film, i|
-      if i
-        puts "#{i}. #{film.title}"
+    input = ''
+    while input != 'exit'
+    puts "***Welcome to the Studio Ghibli Data Gem Project!***"
+    puts "To list all films, enter 'get films'."
+    puts "To quit, type 'exit'."
+    input = gets.chomp
+      case input
+      when 'get films'
+        get_films
+      when "#{title}"
+        film_info
       end
     end
-    puts ""
   end
-
-  def print_film(film)
-    puts ""
-    puts  "-------------- #{film.title} --------------"
-    puts ""
-    puts film.description
-    puts ""
-    puts film.rt_score
-    puts ""
-  end
-
-  def menu
-    input = nil
-    while input != "exit"
-      puts "Select which film you would to learn more about."
-      puts "Type menu to see the list of films again, or type exit to end the program."
-      input = gets.strip.downcase
-      if input.to_i > 0
-        if film = StudioGhibli::Film.find(input.to_i)
-           print_film(film)
+  
+  def get_films
+    Film.all
+        puts "#{title}"
         end
-      elsif input == "menu"
-        list_film
-      else
-        puts "Not sure what you want? Type menu or exit." unless input == "exit"
-      end
     end
-  end
-
-  def goodbye
-    puts "See You Soon!"
-  end
+    puts "See a film you like? Type out the name. Example: Spirited Away"
+  
+      
+  
+  def film_info
+    Film.all.each do |film|
+      puts "#{film.title.upcase}"
+      puts  "#{film.description}"
+      puts "#{film.rt_score}"
+    end
 end
