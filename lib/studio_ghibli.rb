ad30f524653ae5e0a82f7c81586e8d6169f2b0db
Corina Talavera <43788757+Corina-Talavera@users.noreply.github.com>
2018-12-26 22:31:33 -0500

    Update studio_ghibli.rb

--- lib/studio_ghibli.rb
+++ lib/studio_ghibli.rb
@@ -1,7 +1,12 @@
-require "studio_ghibli/version"
-
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require_relative "studio_ghibli/version"
+require_relative 'studio_ghibli/api'
+require_relative 'studio_ghibli/film'
+require_relative 'studio_ghibli/cli'
+require_relative 'studio_ghibli/version'
 module StudioGhibli
   class Error < StandardError; end
   # Your code goes here...
 end
-require_relative '../config/environment'
