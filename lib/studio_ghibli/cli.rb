e37b7f3d082d85302e85d862a0443da25929b5c2
Corina Talavera <43788757+Corina-Talavera@users.noreply.github.com>
2018-12-31 11:55:36 -0500

    Update cli.rb
    Updated CLI and welcome message does work

--- lib/studio_ghibli/cli.rb
+++ lib/studio_ghibli/cli.rb
@@ -1,32 +1,56 @@
+
 class CLI
-  
-  attr_reader :title, :description, :rt_score
-  
   def call
-    input = ''
-    while input != 'exit'
-    puts "***Welcome to the Studio Ghibli Data Gem Project!***"
-    puts "To list all films, enter 'get films'."
-    puts "To quit, type 'exit'."
-    input = gets.chomp
-      case input
-      when 'get films'
-        get_films
-      when "#{title}"
-        film_info
+    welcome
+    list_films
+    menu
+    good_bye
+  end
+
+  def welcome
+    puts ""
+    puts "****** Welcome to the Studio Ghibli Project *******"
+    puts ""
+  end
+
+  def list_films
+    Film.all.each.with_index(1) do |film, i|
+      if i <= 20
+        puts "#{i}. #{film.title}"
+      end
+    end
+    puts ""
+  end
+
+  def print_film(film)
+    puts ""
+    puts  "-------------- #{film.title} --------------"
+    puts ""
+    puts film.description
+    puts ""
+    puts film.rt_score
+    puts ""
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Select which movie you would like to learn about by typing number."
+      puts "Type menu to see the list of movies again, or type exit to end the program."
+      input = gets.strip.downcase
+      if input.to_i > 0
+        if film = StudioGhibli::Film.find(input.to_i)
+           print_film(film)
+        end
+      elsif input == "menu"
+        list_films
+      else
+        puts "Not sure what you want? Type menu or exit." unless input == "exit"
       end
     end
   end
-  
-  def get_films
-    Film.all
-        puts "#{title}"
-        end
-    end
-  def film_info
-    Film.all.each do |film|
-      puts "#{film.title.upcase}"
-      puts  "#{film.description}"
-      puts "#{film.rt_score}"
-    end
+
+  def good_bye
+   puts "Goodbye!"
+  end
 end
