f01823004d4b1d98762a205478b2a1f5741ec3df
Corina Talavera <43788757+Corina-Talavera@users.noreply.github.com>
2019-01-03 20:48:10 -0500

    Update film.rb
    moved the film class from the API and erased double film class

--- lib/studio_ghibli/film.rb
+++ lib/studio_ghibli/film.rb
@@ -1,17 +1,16 @@
+#!/usr/bin/env ruby
+require 'nokogiri'
+require 'open-uri'
+require 'net/http'
+require 'rubygems'
+require 'json'
 class Film
   attr_accessor :title, :description, :rt_score
-
-  @@all = [ ]
-
-  def initialize
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.find(id)
-    self.all[id-1]
+  def initialize(title_hash)
+    title_hash.each do |method,arg|
+      if self.respond_to?("#{method}=")
+        self.send("#{method}=",arg)
+      end
+    end
   end
 end
