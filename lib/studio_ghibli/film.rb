3f80d9101b1887f2c772e58bb589cf9cae15b169
Corina Talavera <43788757+Corina-Talavera@users.noreply.github.com>
2019-01-04 20:48:33 -0500

    Update film.rb
    added @@ and save to film.rb

--- lib/studio_ghibli/film.rb
+++ lib/studio_ghibli/film.rb
@@ -1,11 +1,15 @@
-#!/usr/bin/env ruby
-require 'nokogiri'
-require 'open-uri'
-require 'net/http'
-require 'rubygems'
-require 'json'
 class Film
+  @@all = [ ]
   attr_accessor :title, :description, :rt_score
+
+  def self.all
+    if @@all.empty?
+      create
+    else
+      @@all
+    end
+  end
+
   def initialize(title_hash)
     title_hash.each do |method,arg|
       if self.respond_to?("#{method}=")
@@ -13,4 +17,18 @@ class Film
       end
     end
   end
+
+  def self.create
+    api = API.new
+    results = api.get_film.map do |title_hash|
+      self.new(title_hash).save
+    end
+  end
+
+  def save
+      @@all << self
+      self
+  end
 end
+
+  Film.create
